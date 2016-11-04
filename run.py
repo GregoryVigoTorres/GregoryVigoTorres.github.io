1af3c64172ec69bce58e4301c352565f48c21228
Gregory Vigo Torres <gregory.vigo@gmail.com>
2016-11-04 10:29:23 +0100

    don't track .tags

--- run.py
+++ run.py
@@ -7,11 +7,10 @@ from app import app
 @click.command()
 @click.option('-c', '--config', default='config.py', help='specify config')
 def run(config):
-    print 'using config <%s>' %config
+    print('using config <%s>' %config)
     config_path = os.path.join('instance', config)
     app.config.from_pyfile(config_path)
     app.run()
 
 if __name__ == '__main__':
-
     run()
