e22991f6f42d6a5a99dc1b02b0cd5cbb481edc3e
Gregory Vigo Torres <gregory.vigo@gmail.com>
2016-11-10 15:39:39 +0100

    boxy border style working with mobile

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,32 @@
-
-var topButton = document.getElementById("back-to-top-link");
+var topButton = document.getElementById("back-to-top");
 var foldElem = document.getElementById('top');
 var fold = foldElem.clientHeight;
-
-// var menuButtonContainer = document.getElementById('menu-button-container');
-var menuItemList = document.getElementById('menu-items');
 
 document.onscroll = function(eve) {
     var pos = window.scrollY;
     if (pos > fold) {
         topButton.style.display = 'block';
-        // menuButtonContainer.style.display = 'block';
     } else {
         topButton.style.display = 'none';
-        // menuButtonContainer.style.display = 'none';
     };
 };
 
 var navLinks = document.querySelectorAll('nav > a');
+
+function slowScrollTop(eve) {
+  /*
+   * the other function only scrolls down
+   */
+  function scrollUp(time) {
+    window.scrollBy(0, -175);
+    if (window.scrollY > 0) {
+      return window.requestAnimationFrame(scrollUp);
+    };
+  };
+
+  eve.preventDefault();
+  window.requestAnimationFrame(scrollUp);
+};
 
 function slowScroll(eve) {
   function scrollWindow(time) {
@@ -31,7 +40,7 @@ function slowScroll(eve) {
   var scrollTarget = document.querySelector(scrollTargetId);
 
   if (!scrollTarget) {
-    return
+    return;
   } else {
     eve.preventDefault();
   };
@@ -45,51 +54,4 @@ for (i=0; i<navLinks.length; i++) {
   navLinks[i].addEventListener('click', slowScroll);
 };
 
-// menuButtonContainer.addEventListener('mouseover', function(eve) {
-//     if (menuItemList.style != undefined) {
-//         menuItemList.style.display = 'block';
-//     };
-// });
-//
-// menuButtonContainer.addEventListener('mouseout', function(eve) {
-//     if (menuItemList.style != undefined) {
-//         menuItemList.style.display = 'none';
-//     };
-// });
-//
-// SMALLER SCREENS
-if (window.screen.availWidth < 800) {
-    /* move contact links to new container */
-    // var contactElems = document.getElementById('contact-links');
-    // var contactElemTarget = document.querySelector('#mobile-links-container p');
-    // contactElemTarget.insertAdjacentElement('afterend', contactElems);
-
-    // menuButtonContainer.addEventListener('touchmove', function(eve) {
-    //     for (i=0; i < menuItemList.children.length; i++) {
-    //         // unset background on menu items
-    //         menuItemList.children[i].firstElementChild.classList.remove('fake-hover');
-    //     };
-    //
-    //     var currElem = document.elementFromPoint(eve.touches[0].clientX, eve.touches[0].clientY);
-    //     if (currElem) {
-    //         if (currElem.tagName === 'A' && currElem.className === 'menu-item') {
-    //             currElem.classList.add('fake-hover');
-    //         }
-    //     };
-    //     eve.preventDefault();
-    // }, false);
-
-    // show/hide container
-    // menuButtonContainer.addEventListener('touchstart', function(eve) {
-    //     menuItemList.style.display = 'block';
-    //     menuButtonContainer.style.width = '50%';
-    // }, false);
-    //
-    // for (i=0; i<menuItemList.children.length; i++) {
-    //     menuItemList.children[i].firstElementChild.addEventListener('click', function(eve) {
-    //         menuItemList.style.display = 'none';
-    //         menuButtonContainer.style.width = 'auto';
-    //     }, false);
-    // };
-}; // responsive things
-
+topButton.addEventListener('click', slowScrollTop);
